4b67f2402e76c5682ac40734bf49d4da772b0456
AlenLong <alenlong1@gmail.com>
2022-12-28 19:38:47 -0300

    tp27 Creando una aplicación con React

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer"; 
+import ContentWrapper from "./components/ContentWrapper"; 
+
+
+import './assets/css/app.css'
+
+
+
+function App() {
+  return (
+    <div>
+      <Sidebar/>
+      <ContentWrapper/> 
+      <Footer/> 
+    </div>
+        );
+      }
+
+
+
+export default App;
+
+
+
+
---
+++ src/components/ContentWrapper.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Topbar from './Topbar'
+import ContentRowTop from './ContentRowTop'
+
+function ContentWrapper() {
+    return (
+    <div id="content-wrapper" class="d-flex flex-column">
+    <div id="content">  
+
+        <Topbar/>
+
+
+
+
+    </div>
+    </div>
+    )
+}
+
+export default ContentWrapper
